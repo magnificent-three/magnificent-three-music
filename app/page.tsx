b1c4d1a8b296e19d255e7497955c8c75b75ca67c
Jeff Lane <jeff@jeffreyrobertlane.com>
2025-12-14 17:02:23 -0800

    adding rotating images, smaller fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,34 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex justify-center font-sans bg-black">
-      <main className="flex w-full h-screen max-w-3xl flex-col items-center text-center p-2 bg-black sm:items-start">
-        <h1 className="text-6xl my-10 text-orange-300 w-full text-center font-antique-bold">
+      <main className="flex w-full h-screen max-w-3xl flex-col items-center text-center p-2 bg-black sm:items-start z-0">
+        <h1 className="text-5xl my-8 text-orange-300 w-full text-center font-antique-bold">
           Magnificent Three Music
         </h1>
-        <h2 className="text-5xl mb-10 text-orange-200 w-full text-center font-antique">
+        <h2 className="text-4xl mb-8 text-orange-200 w-full text-center font-antique">
           Story. Composition. Magic.
         </h2>
-        <p className="text-2xl mb-5 text-orange-300 w-full text-justify">
+        <p className="text-xl mb-3 text-orange-300 w-full text-justify">
           What is Magnificent Three Music?
         </p>
-        <p className="text-xl mb-5 text-orange-200 w-full text-justify">
+        <p className="text-lg mb-3 text-orange-200 w-full text-justify">
           We&apos;re a small, independent music publishing company (ASCAP) with
           two artists on our roster, <strong>Heart of Cygnus</strong>{" "}
           (Progressive Metal), and <strong>Burningdown</strong> (Alternative
           Rock).
         </p>
-        <p className="text-2xl mb-5 text-orange-300 w-full text-justify">
+        <p className="text-xl mb-3 text-orange-300 w-full text-justify">
           What are the Magnificent Three?
         </p>
-        <p className="text-xl mb-5 text-orange-200 w-full text-justify">
+        <p className="text-lg mb-3 text-orange-200 w-full text-justify">
           These are the three ingredients that make great music: A compelling{" "}
           <strong>Story</strong>, great <strong>Composition</strong>, and a bit
           of <strong>Magic</strong> from the soul of the artist.
         </p>
-        <p className="text-2xl mb-5 text-orange-300 w-full text-justify">
+        <p className="text-xl mb-3 text-orange-300 w-full text-justify">
           Is Magnificent Three Music a One Stop Shop for licensing?
         </p>
-        <p className="text-xl mb-5 text-orange-200 w-full text-justify">
+        <p className="text-lg mb-3 text-orange-200 w-full text-justify">
           Yes! We own all the rights for our music - songwriter, publishing, and
           sound recording.
         </p>
